fef7f79c311350d8d705896d8ff495037b2475f4
Alfred Arinze <arinze.dibor-alfred@gunsel.com.tr>
2023-07-20 14:06:29 +0300

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // you can customize you publish directory like so
+  build: {
+    outDir: "public"
+  },
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
